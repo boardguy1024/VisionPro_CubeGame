19889d374bf0c8514c4f75ed2addf3353838c9c6
paku <boardguy1024@gmail.com>
2024-02-20 23:25:24 +0900

    Initial Commit

---
+++ Cube/CubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeApp.swift
+//  Cube
+//
+//  Created by paku on 2024/02/20.
+//
+
+import SwiftUI
+
+@main
+struct CubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
